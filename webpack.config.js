5c9c10d5e88c33e11887b1b784c15afc4f0042c6
sebug <sebastian.gfeller@gmail.com>
2018-05-06 13:21:07 +0200

    Using HTML template

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
 	app: './ClientSide/src/index.js',
 	appRoot: './ClientSide/components/app-root/index.js'
     },
+    plugins: [
+	new HtmlWebpackPlugin({
+	    title: 'Expositions to Visit',
+	    template: './ClientSide/src/index.html'
+	})
+    ],
     output: {
 	filename: '[name].bundle.js',
 	path: path.resolve(__dirname, 'wwwroot')
